70ed17c21842176e18a85084ebf1309fc08b242a
Charles-Meldhine Madi Mnemoi <63333367+cmmm976@users.noreply.github.com>
2021-06-28 18:17:04 +0200

    moved to streamlit for the app

---
+++ src/app/main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from back import CustomEncoder,request_categories,request_data
+from sklearn.base import BaseEstimator
+
+#front-end
+st.title('TAS Predictor')
+st.header('Predict the best possible times of your favorite speedgame.')
+st.write('''Your speedgame don't have TASes ? Do you want to know how fast this goofy extension category can be ?
+ 
+This machine learning-driven app will give you answers.''')
+
+game_form =  st.form("game_form")
+game = game_form.text_input('Type a game :')
+game_submitted = game_form.form_submit_button("Get best possible times !")
+if game_submitted:
+    for category in request_categories(game):
+        results = request_data(game,category)
+        WR_link = results['WR_link']
+        WR_time = results['WR_time']
+        predicted_time = results['predicted_time']
+        st.subheader(category + ':')
+        '''
+        [Link to world record]('''+WR_link+''') (time : '''+WR_time+''')
+         
+        **The best possible time is :** '''+predicted_time
+       
+
