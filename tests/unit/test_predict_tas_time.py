e2a544aefdd8a361ab5c0bb13dc468e23a75af42
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2025-12-30 03:32:37 +0100

    refactor: Simplify test by replacing fake functions with inline lambdas

--- tests/unit/test_predict_tas_time.py
+++ tests/unit/test_predict_tas_time.py
@@ -4,26 +4,14 @@ from owrestimator.app.back import TASPrediction, predict_tas_time
 from owrestimator.app.speedrun_com_gateway import GameCategoryWorldRecord
 
 
-def fake_get_game_category_world_record(
-    game: str, category: str
-) -> GameCategoryWorldRecord:
-    return GameCategoryWorldRecord(
-        105.967, 8, 101, "http://www.speedrun.com/deadcells/run/mrd451dz"
-    )
-
-
-def fake_predict_tas_time_from_wr(
-    category_world_record: GameCategoryWorldRecord,
-) -> float:
-    return 100
-
-
 def test_predict_tas_time() -> None:
     assert predict_tas_time(
         "Dead Cells",
         "Any% Warpless",
-        fake_get_game_category_world_record,
-        fake_predict_tas_time_from_wr,
+        lambda game, category: GameCategoryWorldRecord(
+            105.967, 8, 101, "http://www.speedrun.com/deadcells/run/mrd451dz"
+        ),
+        lambda _: 100,
     ) == TASPrediction(
         str(datetime.timedelta(seconds=100)),
         "http://www.speedrun.com/deadcells/run/mrd451dz",
