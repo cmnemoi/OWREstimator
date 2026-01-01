c511e941c7f5265d4312590ad1794ed19ae29af6
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2026-01-01 12:02:16 +0100

    test: adapt test to 2026, less flaky

--- tests/integration/test_speedrun_com_gateway.py
+++ tests/integration/test_speedrun_com_gateway.py
@@ -18,10 +18,7 @@ def test_get_game_category_world_record():
     category_world_record = get_game_category_world_record(
         "Dead Cells", "Any% Warpless"
     )
-    assert category_world_record.time == 105.967
-    assert category_world_record.age == 8
-    assert category_world_record.nb_of_runs == 101
-    assert (
-        category_world_record.weblink
-        == "http://www.speedrun.com/deadcells/run/mrd451dz"
-    )
+    assert isinstance(category_world_record.time, float)
+    assert isinstance(category_world_record.age, int)
+    assert isinstance(category_world_record.nb_of_runs, int)
+    assert category_world_record.weblink.startswith("http://www.speedrun.com/deadcells")
